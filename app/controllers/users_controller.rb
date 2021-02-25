b2ccff7125318e87f2b01c06a376a3044635fecb
fl4m3m4k3r <fl4m3m4k3r@gmail.com>
2021-02-25 12:49:56 +0300

    Views refactoring

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,39 @@
 class UsersController < ApplicationController
+  def index
+    @users = [
+      User.new(
+        id: 1,
+        name: 'Vadim',
+        username: 'installero',
+        avatar_url: 'http://placehold.it/300'
+      ),
+      User.new(
+        id: 2,
+        name: 'Misha',
+        username: 'aristofun'
+      )
+    ]
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @time = Time.now
-    @hello = "Hello!"
+    @user = User.new(
+      name: 'Vadim',
+      username: 'installero',
+      avatar_url: 'https://secure.gravatar.com/avatar/' \
+        '71269686e0f757ddb4f73614f43ae445?s=100'
+    )
+
+    @questions = [
+      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
+      Question.new(text: 'Что делаешь?', created_at: Date.parse('28.03.2016'))
+    ]
+
+    @new_question = Question.new
   end
 end
