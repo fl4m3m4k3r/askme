5800bd5959d6a80547196ad18d6b1f2ee793d15d
fl4m3m4k3r <fl4m3m4k3r@gmail.com>
2021-02-26 12:20:33 +0300

    new helper: word_decline & questions count on user page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,16 @@ module ApplicationHelper
       asset_path 'avatar.webp'
     end
   end
+
+  def word_decline(number, first_form, second_form, third_form)
+    if (number % 100).between?(11, 14)
+      return third_form
+    else
+      case number % 10
+      when 1 then first_form
+      when 2..4 then second_form
+      else third_form
+      end
+    end
+  end
 end
