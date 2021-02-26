cc7192e1b80a2aa52f008bead0ddc0271bb99275
fl4m3m4k3r <fl4m3m4k3r@gmail.com>
2021-02-26 14:41:53 +0300

    temp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,27 @@
 class UsersController < ApplicationController
   def index
-    @users = [
-      User.new(
-        id: 1,
-        name: 'Vadim',
-        username: 'installero',
-        avatar_url: 'http://placehold.it/300'
-      ),
-      User.new(
-        id: 2,
-        name: 'Misha',
-        username: 'aristofun'
-      )
-    ]
+    @users = User.all
   end
 
   def new
+    @user = User.new
   end
 
   def edit
   end
 
+  def create
+    @user = User.new(user_params)
+
+    @user.save
+  end
+
   def show
-    @user = User.new(
-      name: 'Vadim',
-      username: 'installero',
-      avatar_url: 'https://secure.gravatar.com/avatar/'\
-        '71269686e0f757ddb4f73614f43ae445?s=100'
-    )
+  end
 
-    @questions = [
-      Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
-      Question.new(text: 'Что делаешь?', created_at: Date.parse('28.03.2016'))
-    ]
-
-    @new_question = Question.new
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation,
+                                 :name, :username, :avatar_url)
   end
 end
