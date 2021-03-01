e091f58e3f7a16479728a87d8624994c7f02b6d6
fl4m3m4k3r <fl4m3m4k3r@gmail.com>
2021-03-01 16:47:12 +0300

    users & sessions controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
+  before_action :load_user, except: [:index, :create, :new]
+
+  before_action :authorize_user, except: [:index, :new, :create, :show]
+
   def index
     @users = User.all
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new
   end
 
@@ -11,24 +16,41 @@ class UsersController < ApplicationController
   end
 
   def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+
     @user = User.new(user_params)
 
-<<<<<<< HEAD
-    @user.save
-=======
     if @user.save
-      session[:user_id] = @user.id
       redirect_to root_url, notice: 'Вы успешно зарегистрированы!'
     else
       render 'new'
     end
->>>>>>> 0c37b7b... temp
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные успешно обновлены'
+    else
+      render 'edit'
+    end
   end
 
   def show
+    @questions = @user.questions.order(created_at: :desc)
+
+    @new_question = @user.questions.build
   end
 
   private
+
+  def authorize_user
+    reject_user unless @user == current_user
+  end
+
+  def load_user
+    @user ||= User.find params[:id]
+  end
+
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation,
                                  :name, :username, :avatar_url)
