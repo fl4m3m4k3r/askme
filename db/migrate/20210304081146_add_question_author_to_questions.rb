2b6b574b3d48e594d6b30bcab6b4d35cb596d6b4
fl4m3m4k3r <fl4m3m4k3r@gmail.com>
2021-03-04 13:38:35 +0300

    migration: added qustion author to Question. _question now show who asked question. some improvements and fixes of _question view

---
+++ db/migrate/20210304081146_add_question_author_to_questions.rb
@@ -0,0 +1,5 @@
+class AddQuestionAuthorToQuestions < ActiveRecord::Migration[6.1]
+  def change
+    add_column :questions, :author, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_03_131840) do
+ActiveRecord::Schema.define(version: 2021_03_04_081146) do
 
   create_table "questions", force: :cascade do |t|
     t.string "text"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_03_03_131840) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id", null: false
+    t.string "author"
     t.index ["user_id"], name: "index_questions_on_user_id"
   end
 
