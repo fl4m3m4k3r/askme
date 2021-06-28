d0399e5d683b5256d19b4e68da8f7d3eb8026df3
fl4m3m4k3r <fl4m3m4k3r@gmail.com>
2021-06-29 00:56:48 +0300

    Implemented hashtags row at root page && page with list of questions with hashtag

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -2,5 +2,6 @@ class HashtagsController < ApplicationController
   def show
     hashtag = Hashtag.with_questions.find_by!(text: params[:text])
     @questions = hashtag.questions
+    @hashtags = Hashtag.all
   end
 end
